51f93e1c9be3f9c42349a3cec2d4594d33c0d0af
Alexandr <87748202+shadowHD-bit@users.noreply.github.com>
2022-03-25 16:09:55 +0700

    added component header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,11 @@
-import React, {useContext } from 'react'
+import React, {useState, useContext } from 'react'
 import { Context } from '../..'
 
 //import style Bootstrap
 import './Header.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import {Offcanvas, Dropdown} from 'react-bootstrap'
 import { BsFacebook } from 'react-icons/bs'; 
 import { BsTwitter } from 'react-icons/bs'; 
 import { BsGoogle } from 'react-icons/bs'; 
@@ -13,6 +14,12 @@ import { FaVk } from 'react-icons/fa';
 import {MdMenu} from 'react-icons/md';
 
 export default function Header() {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     const {user} = useContext(Context)
 
     return (  
@@ -21,7 +28,7 @@ export default function Header() {
       <div className='container h-100'>
           <div className='row align-items-center justify-content-center align-middle'>
               <div className='col'>
-                  <a href='/'><img src='src/img/header/svg_default_logo.svg'/></a>
+                  <a href='/'><img src="../../img/header/default_logo.png"/></a>
               </div>
               <div className='col text-right'>
                   <div className='col social_header_block'>
@@ -31,57 +38,61 @@ export default function Header() {
                       <a><FaVk className='social_header last' size={'30px'}/></a>
                       
 
-                  <a className='right_menu' type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><MdMenu className='mdmenu'    size={'30px'}/></a>
+                  <a className='right_menu' type="button" onClick={handleShow} ><MdMenu className='mdmenu'size={'30px'}/></a>
+                  
                   </div>
-                                          
+
+                  <Offcanvas className='Offs' placement='end' show={show} onHide={handleClose}>
+                    <div className="offcanvas-header">
+                      <h5 id="offcanvasRightLabel">Be sure of yourself...</h5>
+                  </div>
+                  <div className="offcanvas-body">
+                      <a href='/'><button className="btn btn-outline-danger mt-3" type="button">
+                          Главная
+                      </button></a>
+
+                      <Dropdown className="dropdown mt-3">
+                      <Dropdown.Toggle variant='none' className="btn btn-outline-danger dropdown-toggle">
+                        Товары
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu className="dropdown-menu">
+                        <Dropdown.Item className="dropdown-item" href="#/action-1">Мужская одежда</Dropdown.Item>
+                        <Dropdown.Item className="dropdown-item" href="#/action-2">Женская одежда</Dropdown.Item>
+                        <Dropdown.Item className="dropdown-item" href="#/action-3">Обувь</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+
+                      <button className="btn btn-outline-danger mt-3" type="button" href="/shop-news">
+                          Новости
+                      </button>
+
+                      <Dropdown className="dropdown mt-3">
+                      <Dropdown.Toggle variant='none' className="btn btn-outline-danger dropdown-toggle">
+                        Прочее
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu className="dropdown-menu">
+                        <Dropdown.Item className="dropdown-item" href="#/action-1">О нас</Dropdown.Item>
+                        <Dropdown.Item className="dropdown-item" href="#/action-2">Контакты</Dropdown.Item>
+                        <Dropdown.Item className="dropdown-item" href="#/action-3">Основные адреса</Dropdown.Item>
+                        <Dropdown.Item className="dropdown-item" href="#/action-3">Правила</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+
+                      <div className='offcanvas-bottom'>
+                      <a href='/auth'><button className="btn btn-outline-success fixed-bottom" type="button" href="/shop-news">
+                          Вход / Регистрация
+                      </button></a>
+                      </div>
+                    </div>
+                  </Offcanvas>    
+
               </div>
           </div>
       </div>
 
-      <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-      <div className="offcanvas-header">
-          <h5 id="offcanvasRightLabel">Be sure of yourself...</h5>
-          <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Закрыть"></button>
-      </div>
-      <div className="offcanvas-body">
-          <a href='/'><button className="btn btn-outline-danger mt-3" type="button">
-              Главная
-          </button></a>
-
-          <div className="dropdown mt-3">
-          <button className="btn btn-outline-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-              Товары
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <li><a className="dropdown-item" href="#">Мужская одежда</a></li>
-              <li><a className="dropdown-item" href="#">Женская одежда</a></li>
-              <li><a className="dropdown-item" href="#">Обувь</a></li>
-          </ul>
-          </div> 
-
-          <button className="btn btn-outline-danger mt-3" type="button" href="/shop-news">
-              Новости
-          </button>
-
-          <div className="dropdown mt-3">
-          <button className="btn btn-outline-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-              Прочее
-          </button>
-          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <li><a className="dropdown-item" href="/about-us">О нас</a></li>
-              <li><a className="dropdown-item" href="/contact">Контакты</a></li>
-              <li><a className="dropdown-item" href="/contact-list">Основные адреса</a></li>
-              <li><a className="dropdown-item" href="#">Правила</a></li>
-          </ul>
-          </div>
-
-          <div className='offcanvas-bottom'>
-          <a href='/auth'><button className="btn btn-outline-success fixed-bottom" type="button" href="/shop-news">
-              Вход / Регистрация
-          </button></a>
-          </div>
-          </div>
-      </div>
   </div>
+  
   )
 }
